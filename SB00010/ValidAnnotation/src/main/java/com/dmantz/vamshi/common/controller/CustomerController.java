691fd326220e28faa1f9aba180d5b706c51bd20f
Vamshi1233338 <bhairojuvamshi01444@gmail.com>
2021-12-23 14:57:14 +0530

    VALID

--- SB00010/ValidAnnotation/src/main/java/com/dmantz/vamshi/common/controller/CustomerController.java
+++ SB00010/ValidAnnotation/src/main/java/com/dmantz/vamshi/common/controller/CustomerController.java
@@ -4,17 +4,18 @@ import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dmantz.vamshi.common.entity.Customer;
 
 @RestController
 public class CustomerController {
-	@PostMapping("/test")
+	@RequestMapping(value = "testing", method = RequestMethod.POST)
 	public String test(@Valid@RequestBody Customer customer)
 	{
 		return "success";
 	}
 
 }
-   
--- SB00010/ValidAnnotation/src/main/java/com/dmantz/vamshi/common/entity/Customer.java
+++ SB00010/ValidAnnotation/src/main/java/com/dmantz/vamshi/common/entity/Customer.java
@@ -1,11 +1,18 @@
 package com.dmantz.vamshi.common.entity;
 
+import javax.validation.constraints.Email;
+//import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class Customer {
 	@NotNull
+	@Email
+	@Size(min=17)  
 	private String customerName;
 	@NotNull
+	@Pattern(regexp="^[a-zA-Z0-9]{8}",message="length must be 8")  
 	private String password;
 	public String getCustomerName() {
 		return customerName;
